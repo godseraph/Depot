a63a51f5fa9758866ff3f57c96efb1b6dccbf715
wjc <wjc@wjc-virtual-machine.(none)>
2012-09-30 07:48:43 +0800

    start A1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '1e01175476514f48dfa21b12af0f5d98871f089c9806849d3d7e0426939176a8155b261ccdc2eed89618861d9e39bb9e3ddb881cac05a2713472834d5518a960'
